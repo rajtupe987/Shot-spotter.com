fb6678cc6be340fd991e0af742cbee49bbf3327b
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-06-16 12:29:56 +0530

    fw21_1031 day-4

--- Backend/cli/routes/photographers.router.js
+++ Backend/cli/routes/photographers.router.js
@@ -1,156 +1,158 @@
-const express=require("express");
+const express = require("express");
 
-const { photographers } = require("../models/index");
+const { photographers, sequelize } = require("../models/index");
 const { Op } = require('sequelize');
 
-const photoRouter=express.Router();
+const photoRouter = express.Router();
 
 
 // Read all photographers
+//localhost:4002/api/photographers -- > normal get
+//localhost:4002/api/photographers?sort=asc -->sort asc
+//localhost:4002/api/photographers?sort=desc -->sort desc
+
 photoRouter.get("/", async (req, res) => {
-    try {
-      const data = await photographers.findAll();
-      res.status(200).json({
-        isError: false,
-        data,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        isError: true,
-        error,
-      });
-    }
-  });
-
-
+  const { sort } = req.query;
+  
+  try {
+    let order = [];
     
-  photoRouter.post("/", async (req, res) => {
-    try {
-      const data = await photographers.create({
-        ...req.body,
-      });
-      res.status(200).json({
-        isError: false,
-        data,
-      });
-    } catch (error) {
-      res.status(404).json({
-        isError: true,
-        error,
-      });
-      console.log(error)
+    if (sort === "asc") {
+      order.push(["price", "ASC"]);
+    } else if (sort === "desc") {
+      order.push(["price", "DESC"]);
     }
-  });
+    
+    const data = await photographers.findAll({
+      order: order
+    });
+    
+    res.status(200).json({
+      isError: false,
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      isError: true,
+      error,
+    });
+  }
+});
 
 
+//post method for photogrhphers
+photoRouter.post("/", async (req, res) => {
+  try {
+    const data = await photographers.create({
+      ...req.body,
+    });
+    res.status(200).json({
+      isError: false,
+      data,
+    });
+  } catch (error) {
+    res.status(404).json({
+      isError: true,
+      error,
+    });
+    console.log(error)
+  }
+});
 
-  photoRouter.delete("/:id", async (req, res) => {
-    try {
-      const data = await photographers.destroy({
-        where: {
-          id: req.params.id,
-        },
-      });
-      res.status(200).json({
-        isError: false,
-        data,
-      });
-    } catch (error) {
-      res.status(404).json({
-        isError: true,
-        error,
-      });
-      console.log(error)
-    }
-  });
-  
 
-  photoRouter.put("/:id", async (req, res) => {
-    try {
-      const data = await photographers.upsert({
+//delete method
+photoRouter.delete("/:id", async (req, res) => {
+  try {
+    const data = await photographers.destroy({
+      where: {
         id: req.params.id,
-        ...req.body,
-      });
-      res.status(200).json({
-        isError: false,
-        data,
-      });
-    } catch (error) {
-      res.status(404).json({
-        isError: true,
-        error,
-      });
-    }
-  });
+      },
+    });
+    res.status(200).json({
+      isError: false,
+      data,
+    });
+  } catch (error) {
+    res.status(404).json({
+      isError: true,
+      error,
+    });
+    console.log(error)
+  }
+});
 
- 
-  photoRouter.get('/search', async (req, res) => {
-    const { location } = req.query;
-  
-    try {
-      const photographersData = await photographers.findAll({
-        where: {
-          location: {
-            [Op.like]: `%${location}%` // Filter by location
-          }
-        }
-      });
-  
-      res.status(200).json(photographersData);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: 'An error occurred while fetching photographers.' });
-    }
-  });
-  
+//update/put method
+photoRouter.put("/:id", async (req, res) => {
+  try {
+    const data = await photographers.upsert({
+      id: req.params.id,
+      ...req.body,
+    });
+    res.status(200).json({
+      isError: false,
+      data,
+    });
+  } catch (error) {
+    res.status(404).json({
+      isError: true,
+      error,
+    });
+  }
+});
 
-  photoRouter.get('/search', async (req, res) => {
-    const { category } = req.query;
+//////////all about sorting and filtering/////////
+
+//localhost:4002/api/photographers/search?location=New York&sort=desc
+
+//localhost:4002/api/photographers/search?location=New York&sort=asc
+
+//localhost:4002/api/photographers/search?location=New York
+
+//localhost:4002/api/photographers/search?category=Portraits
+
+photoRouter.get('/search', async (req, res) => {
+  const { category, location ,sort } = req.query;
   
-    try {
-      const photographersData = await photographers.findAll({
-        where: {
-          category: {
-            [Op.like]: `%${category}%` // Filter by category
-          }
-        }
-      });
-      
-      res.status(200).json(photographersData);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: 'An error occurred while fetching photographers.' });
+  try {
+    let photographersData;
+    let order = [];
+
+    if (sort === "asc") {
+      order.push(["price", "ASC"]);
+    } else if (sort === "desc") {
+      order.push(["price", "DESC"]);
     }
-  });
-  
-  
-  photoRouter.get('/search', async (req, res) => {
-    const { category, location } = req.query;
-  
-    try {
-      const photographersData = await photographers.findAll({
+
+    if (location && category) {
+      photographersData = await photographers.findAll({
         where: {
-          category: {
-            [Op.like]: `%${category}%` // Filter by category
-          },
-          location: {
-            [Op.like]: `%${location}%` // Filter by location
-          },
-          order: [
-            ['price', 'ASC'] // Sort by price in ascending order
+          [Op.and]: [
+            sequelize.literal(`JSON_CONTAINS(location, '["${location}"]')`),
+            sequelize.literal(`JSON_CONTAINS(category, '["${category}"]')`)
           ]
-        }
+        },
+        order: order
       });
-  
-      res.status(200).json(photographersData);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: 'An error occurred while fetching photographers.' });
+      //console.log(photographersData)
+    } else if (location) {
+      photographersData = await photographers.findAll({
+        where: sequelize.literal(`JSON_CONTAINS(location, '["${location}"]')`),
+        order: order
+      });
+    } else if (category) {
+      photographersData = await photographers.findAll({
+        where: sequelize.literal(`JSON_CONTAINS(category, '["${category}"]')`),
+        order: order
+      });
     }
-  });
-  
+    
+    res.status(200).json(photographersData || []);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching photographers.' });
+  }
+});
 
-  
 
-  module.exports=photoRouter
+module.exports = photoRouter
