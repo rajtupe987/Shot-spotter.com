3b6ea73dbada1fd0041b7c8f3cf19a9a9ef6c888
Anshuman Patel <patelanshuman6@gmail.com>
2023-06-16 15:40:42 +0530

    frontend folder name spelling rectified

--- Fontend/src/App.jsx
+++ Fontend/src/App.jsx
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/home/Home';
+import LoginPage from './Pages/auth/Login';
+import RegisterPage from './Pages/auth/Register';
+import StudioPage from './Pages/studio/StudioPage';
+import AboutPage from './Pages/studio/StudioAbout';
+import AlbumsPage from './Pages/studio/StudioAlbums';
+import PackagesPage from './Pages/studio/StudioPackages';
+import ReviewsPage from './Pages/studio/StudioReviews';
+import AccountPage from './Pages/account/Account';  //review folder nameCase
+import BookingsPage from './Pages/account/Bookings';
+import ShortlistPage from './Pages/account/Shortlist';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/studio" element={<StudioPage />} />
+        <Route path="/studio/:studio_id/about" element={<AboutPage />} />
+        <Route path="/studio/:studio_id/albums" element={<AlbumsPage />} />
+        <Route path="/studio/:studio_id/packages" element={<PackagesPage />} />
+        <Route path="/studio/:studio_id/reviews" element={<ReviewsPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/bookings" element={<BookingsPage />} />
+        <Route path="/shortlist" element={<ShortlistPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
--- Fontend/src/Pages/studio/StudioPage.jsx
+++ Fontend/src/Pages/studio/StudioPage.jsx
@@ -1,1 +1,125 @@
-// Product page to render all photographers
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const StudioPage = () => {
+  const [categories, setCategories] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [photographers, setPhotographers] = useState([]);
+
+  useEffect(() => {
+    // Fetch categories from the backend API
+    fetch('https://your-api-url/categories')
+      .then((response) => response.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.log(error));
+
+    // Fetch locations from the backend API
+    fetch('https://your-api-url/locations')
+      .then((response) => response.json())
+      .then((data) => setLocations(data))
+      .catch((error) => console.log(error));
+
+    // Fetch photographers from the backend API
+    fetch('https://your-api-url/photographers')
+      .then((response) => response.json())
+      .then((data) => setPhotographers(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+
+      <div className="filter-box">
+        {/* Category filter */}
+        <div className="filter-section">
+          <h3>Categories</h3>
+          {categories.map((category) => (
+            <div key={category.id}>
+              <input type="radio" name="category" value={category.name} />
+              <label>{category.name}</label>
+            </div>
+          ))}
+        </div>
+
+        {/* Location filter */}
+        <div className="filter-section">
+          <h3>Locations</h3>
+          {locations.map((location) => (
+            <div key={location.id}>
+              <input type="radio" name="location" value={location.name} />
+              <label>{location.name}</label>
+            </div>
+          ))}
+        </div>
+
+        {/* Price filter */}
+        <div className="filter-section">
+          <h3>Price Range</h3>
+          <div>
+            <input type="radio" name="price" value="0-100" />
+            <label>$0 - $100</label>
+          </div>
+          <div>
+            <input type="radio" name="price" value="100-500" />
+            <label>$100 - $500</label>
+          </div>
+          <div>
+            <input type="radio" name="price" value="500-1000" />
+            <label>$500 - $1000</label>
+          </div>
+          <div>
+            <input type="radio" name="price" value="1000+" />
+            <label>$1000+</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="photographers-list">
+        {photographers.map((photographer) => (
+          <div key={photographer.id} className="card">
+            <div className="card-header">
+              <div className="studio-info">
+                <div className="studio-image">
+                  <img src={photographer.image} alt="Photographer" />
+                </div>
+                <div className="studio-details">
+                  <h4>{photographer.name}</h4>
+                  <p>{photographer.address}</p>
+                  <p>{photographer.phoneNumber}</p>
+                  <div className="categories">
+                    {photographer.categories.map((category) => (
+                      <span key={category.id} className="category-bubble">
+                        {category.name}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="price-info">
+                <div>
+                  <p>Starts from:</p>
+                  <p>{photographer.price}</p>
+                </div>
+                <div>
+                  <p>{photographer.votes} Votes</p>
+                  <p>
+                    Star symbol {photographer.rating}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="image-container">
+              {photographer.images.map((image) => (
+                <img key={image.id} src={image.url} alt="Photographer" />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StudioPage;
