1d5e4703d960d3e4ea75c6afff6b6925a8675d80
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-11-23 11:14:15 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -23,18 +23,11 @@ app.use(express.json());
 app.use(userRoute);
 app.use("/auth", authRoute);
 app.use("/studio", photographerRouter);
-
 app.use("/auth", authRoute);
-
 app.use("/bookings", bookingRouter);
 
 
-
-
-/*************************************Razorpay********************************************************* */
-
 // razorpay routes please dont touch these routes
-
 const razorpayInstance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
@@ -47,36 +40,6 @@ app.get('/', (req, res) => {
   res.send("welcome to shotspotter")
 })
 
-// app.post('/createOrder', (req, res)=>{
-//   const {amount, currency, receipt, notes} = req.body;
-//   razorpayInstance.orders.create({amount, currency, receipt, notes},
-//       (err, order)=>{
-//           if(!err) {
-//               console.log(order.id)
-//               res.json(order)
-//           } else {
-//               res.send(err);
-//           }
-//       }
-//   )
-// });
-
-// app.post('/verifyOrder', (req, res)=>{
-//   const {order_id, payment_id} = req.body;
-//   const razorpay_signature = req.headers['x-razorpay-signature'];
-//   const key_secret = process.env.RAZORPAY_KEY_SECRET;
-//   let hmac = crypto.createHmac('sha256', key_secret);
-//   hmac.update(order_id + "|" + payment_id);
-//   const generated_signature = hmac.digest('hex');
-//   if(razorpay_signature === generated_signature) {
-//       res.json({success:true, message:"Payment has been verified"})
-//   } else {
-//       res.json({success:false, message:"Payment verification failed"})
-//   }
-// });
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.listen(port, async () => {
   try {
